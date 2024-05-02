28a50e42c812fc7382f66384383e2f75f47468bd
BigJ18 <130506584+BigJ18@users.noreply.github.com>
2024-05-02 09:48:25 -0400

    Add files via upload

---
+++ quick.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+void swap(int* a, int* b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+void quickSortRecursive(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSortRecursive(arr, low, pi - 1);
+        quickSortRecursive(arr, pi + 1, high);
+    }
+}
+
+void printArray(int arr[], int size) {
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11, 90, 45, 77, 88, 33};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printf("Array original: \n");
+    printArray(arr, n);
+    quickSortRecursive(arr, 0, n - 1);
+    printf("\nArray ordenado: \n");
+    printArray(arr, n);
+    return 0;
+}
